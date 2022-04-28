1002b9242471db3945833e63aede85b6f271e4f7
Ruan <occult.aiwass@gmail.com>
2022-04-28 11:26:07 -0300

    add: alert/error return added

--- paletter_web/index/views.py
+++ paletter_web/index/views.py
@@ -10,18 +10,31 @@ from .utils.upload import checkExtensions
 
 def index(request):
     if request.method != "POST":
-        return render(request, "upload.html")
+        return render(request,
+                      "upload.html",
+                      context=None)
 
     if not request.FILES["file"]:
-        return
+        contexts = {
+            "alert": "Arquivo não reconhecido!"
+        }
+        return render(request,
+                      "upload.html",
+                      context=contexts)
 
     f = request.FILES["file"]
     filename = f.name
 
     if not checkExtensions(filename):
-        return
+        contexts = {
+            "alert": "Extensão não-permitida!"
+        }
+        return render(request,
+                      "upload.html",
+                      context=contexts)
 
-    img = Paletter(BytesIO(f.read()))
+    bytes = BytesIO(f.read())
+    img = Paletter(bytes)
     palettes = img.get_image()
 
     response = HttpResponse(content_type="image/png")
